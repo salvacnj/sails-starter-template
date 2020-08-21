9818edfb9cf765a09f96108ad29a248673855944
salvador_criado <salvador.criado@nazaries.com>
2020-08-21 12:16:03 +0200

    api prefix endpoint added

--- config/autoreload.js
+++ config/autoreload.js
@@ -6,7 +6,8 @@ module.exports.autoreload = {
     "api/models",
     "api/controllers",
     "api/services",
-    "config/locales"
+    "config/locales",
+    "config"
   ],
   ignored: [
     // Ignore all files with .ts extension
